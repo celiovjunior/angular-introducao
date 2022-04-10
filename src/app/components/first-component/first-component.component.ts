73df87c714f58e21969811b56d5a61160689fd69
Celio Junior <celio_cjr@hotmail.com>
2022-04-10 17:39:43 -0300

    testando dados dinamicos dentro do componente

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
-
+  name: string = 'Celio';
+  age: number = 28;
+  job: string = 'Dev'
   constructor() { }
 
   ngOnInit(): void {
